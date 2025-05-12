af9fb654246860348a2cd8ce312bd737de9c51af
Sidney Aulakh <sidneyaulakh@gmail.com>
2025-05-12 12:08:43 +1000

    main screen, primary button, fontawesome

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,23 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+
+import Button from '@/components/Button';
+import PrimaryButton from '@/components/PrimaryButton';
+
+import ImageViewer from "@/components/imageViewer";
+
+const PlaceHolderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>Goto About Screen</Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceHolderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <PrimaryButton label="Choose a photo" theme="primary" />
+        <Button label="Use this photo" />
+
+      </View>
     </View>
   );
 }
@@ -15,14 +27,12 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  text: {
-    color: '#ffffff'
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: '#fff'
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
   }
 });
